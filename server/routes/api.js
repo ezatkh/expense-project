6446a09b171fcb11c072a17a16589024946f7db6
ezzat <ezatce117@outlook.com>
2022-08-04 15:30:18 +0300

    add dynamic parameter to get router

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,11 +4,34 @@ const Expense = require("../../model/Expense");
 const moment = require("moment");
 
 router.get("/expense", function (request, response) {
-  Expense.find({})
-    .sort({ date: -1 })
-    .exec(function (err, expenses) {
+  const d1Parameter = request.body.d1;
+  const d2Parameter = request.body.d2;
+  if (d2Parameter && d1Parameter) {
+    Expense.find({
+      $and: [
+        { date: { $gt: moment(d1Parameter).format("LLLL") } },
+        { date: { $lt: moment(d2Parameter).format("LLLL") } },
+      ],
+    }).exec(function (err, expenses) {
       response.send(expenses);
     });
+  } else if (d1Parameter) {
+    const dateNow = moment().format("LLLL");
+    Expense.find({
+      $and: [
+        { date: { $gt: moment(d1Parameter).format("LLLL") } },
+        { date: { $lt: dateNow } },
+      ],
+    }).exec(function (err, expenses) {
+      response.send(expenses);
+    });
+  } else {
+    Expense.find({})
+      .sort({ date: -1 })
+      .exec(function (err, expenses) {
+        response.send(expenses);
+      });
+  }
 });
 
 router.post("/expense", function (request, response) {
